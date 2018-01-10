63a1cffafeb9f9d226782212555404776a16176e
Daeron <gustavo.wy@gmail.com>
2018-01-09 23:54:13 -0200

    Ajustes no endpont /categorias/{id}

--- src/main/java/com/souza/cursomc/services/CategoriaService.java
+++ src/main/java/com/souza/cursomc/services/CategoriaService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.souza.cursomc.domain.Categoria;
+import com.souza.cursomc.exceptions.ObjectNotFoundException;
 import com.souza.cursomc.repositories.CategoriaRepository;
 
 @Service
@@ -12,9 +13,12 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
-		
+	public Categoria buscar(Integer id) {		
 		Categoria obj = repo.findOne(id);
+		if (obj ==null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;		
 	}
 }
