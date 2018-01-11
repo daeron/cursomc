226b35a6a574a51368612da0c027c46f0e4940b6
Daeron <gustavo.wy@gmail.com>
2018-01-11 00:42:00 -0200

    pedido, estadopagamento e pagamento

--- src/main/java/com/souza/cursomc/CursomcApplication.java
+++ src/main/java/com/souza/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.souza.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.souza.cursomc.domain.Cidade;
 import com.souza.cursomc.domain.Cliente;
 import com.souza.cursomc.domain.Endereco;
 import com.souza.cursomc.domain.Estado;
+import com.souza.cursomc.domain.Pagamento;
+import com.souza.cursomc.domain.PagamentoComBoleto;
+import com.souza.cursomc.domain.PagamentoComCartao;
+import com.souza.cursomc.domain.Pedido;
 import com.souza.cursomc.domain.Produto;
+import com.souza.cursomc.domain.enums.EstadoPagamento;
 import com.souza.cursomc.domain.enums.TipoCliente;
 import com.souza.cursomc.repositories.CategoriaRepository;
 import com.souza.cursomc.repositories.CidadeRepository;
 import com.souza.cursomc.repositories.ClienteRepository;
 import com.souza.cursomc.repositories.EnderecoRepository;
 import com.souza.cursomc.repositories.EstadoRepository;
+import com.souza.cursomc.repositories.PagamentoRepository;
+import com.souza.cursomc.repositories.PedidoRepository;
 import com.souza.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner{
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -61,13 +75,9 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-
-
-		
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
 		
-
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 		
@@ -91,6 +101,22 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 10:32"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
 
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1, ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
+		
+		
 	}
 }
---
+++ src/main/java/com/souza/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.souza.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.souza.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
