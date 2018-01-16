f0a898adeeb74eba06c87d608e78318c15a583aa
Daeron <gustavo.wy@gmail.com>
2018-01-16 01:18:39 -0200

    Correcoes update em CategoriaService e construtor de pagamento

--- src/main/java/com/souza/cursomc/services/CategoriaService.java
+++ src/main/java/com/souza/cursomc/services/CategoriaService.java
@@ -36,8 +36,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj =  find(obj.getId());
+		updateDate(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -63,5 +64,8 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
 		
+	}
+	private void updateDate(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
