5d420e25a0eb0f442037502cc1f5b430d9e2570c
Daeron <gustavo.wy@gmail.com>
2018-01-16 01:07:47 -0200

    put, delete e get para cliente

---
+++ src/main/java/com/souza/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,50 @@
+package com.souza.cursomc.dto;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.souza.cursomc.domain.Cliente;
+
+public class ClienteDTO {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatorio")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatorio")
+	@Email(message="Email inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
--- src/main/java/com/souza/cursomc/services/ClienteService.java
+++ src/main/java/com/souza/cursomc/services/ClienteService.java
@@ -1,9 +1,15 @@
 package com.souza.cursomc.services;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
-
 import com.souza.cursomc.domain.Cliente;
+import com.souza.cursomc.dto.ClienteDTO;
+import com.souza.cursomc.exceptions.DataIntegrityException;
 import com.souza.cursomc.exceptions.ObjectNotFoundException;
 import com.souza.cursomc.repositories.ClienteRepository;
 
@@ -20,5 +26,40 @@ public class ClienteService {
 					+ ", Tipo: " + Cliente.class.getName());
 		}
 		return obj;		
+	}
+	public Cliente update(Cliente obj) {
+		Cliente newObj =  find(obj.getId());
+		updateDate(newObj, obj);
+		return repo.save(newObj);
+	}
+
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir por que a entidades relacionadas");
+		}
+		repo.delete(id);
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+		}
+	
+	private void updateDate(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getNome());		
 	}
 }
